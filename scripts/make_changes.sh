b9685e89eda0acbba958883df425af534af37001
tjdetwill007 <tjdetwill007@gmail.com>
2024-02-04 02:45:13 +0530

    update

--- scripts/make_changes.sh
+++ scripts/make_changes.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 echo "Copying files to server"
-cp -rf /home/ec2-user/dist/dist/* /usr/share/nginx/html/
+cp -rf /home/ec2-user/dist/* /usr/share/nginx/html/
 if [ $? -eq 0 ]; then
   echo "Success copy"
 else
