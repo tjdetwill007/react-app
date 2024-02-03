b47ab9f055af79ba2684bf168382d4c4e7f1483f
tjdetwill007 <tjdetwill007@gmail.com>
2024-02-04 03:07:00 +0530

    changes

--- scripts/make_changes.sh
+++ scripts/make_changes.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
-echo "Copying files to server"
-cp -rf /home/ec2-user/dist/* /usr/share/nginx/html/
-if [ $? -eq 0 ]; then
-  echo "Success copy"
-else
-  echo "Copy failed"
-  exit 1
-fi
 echo "Running server"
 systemctl start nginx
+if [ $? -eq 0 ]; then
+  echo "server started"
+else
+  echo "Error occured"
+  exit 1
+fi
